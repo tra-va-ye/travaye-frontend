67faff4dbc69934a3de4f102334264cecd4bd56d
Izima01 <kingsleyizima@gmail.com>
2024-09-15 23:42:45 +0100

    Firebase messaging ask permission

--- src/Layout/RequireAuth.jsx
+++ src/Layout/RequireAuth.jsx
@@ -5,7 +5,7 @@ import Loader from "../components/UI/Loader";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { logout } from "../redux/Slices/authSlice";
-// import { saveDeviceMessagingToken } from "../firebase/messaging";
+import { saveDeviceMessagingToken } from "../firebase/messaging";
 
 const RequireAuth = () => {
   const dispatch = useDispatch();
@@ -13,9 +13,9 @@ const RequireAuth = () => {
   const { isSuccess, isLoading, data } = useGetMeQuery({ userType: sessionStorage.getItem("userType") });
 
   useEffect(() => {
-    // if (isSuccess && !isLoading) {
-    //   saveDeviceMessagingToken(data?.user?._id);
-    // }
+    if (isSuccess && !isLoading) {
+      saveDeviceMessagingToken(data?.user?._id);
+    }
     if (!isSuccess && !isLoading) {
       console.log("no user");
       dispatch(logout());
