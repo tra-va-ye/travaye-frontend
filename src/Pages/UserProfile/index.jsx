a1e6332227a49b7edbed40ba47445af918f30414
Izima01 <kingsleyizima@gmail.com>
2025-06-13 13:49:45 +0100

    Changed QR code link

--- src/Pages/UserProfile/index.jsx
+++ src/Pages/UserProfile/index.jsx
@@ -1,19 +1,19 @@
-import { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import styled from "styled-components";
-import { Button } from "../../components/UI/Buttons";
-import LocationBox from "../../components/UI/Location/LocationBox";
-import { useGetLocationsQuery } from "../../redux/Api/locationApi";
-import { notification } from "antd";
-import ChatIcon from "../../assets/Icons/ChatIcon";
-import { Main } from "../BusinessProfile";
-import ScanIcon from "../../assets/Icons/ScanIcon";
-import SupportModal from "../../components/UI/Modal/UserSupportModal";
-import { TogleButton } from "../../components/Layout/BusinessSidebar";
-import { BsBoxArrowInLeft } from "react-icons/bs";
-import UserSidebar from "../../components/Layout/UserSidebar";
-import { Html5Qrcode } from "html5-qrcode";
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
+import { Button } from '../../components/UI/Buttons';
+import LocationBox from '../../components/UI/Location/LocationBox';
+import { useGetLocationsQuery } from '../../redux/Api/locationApi';
+import { notification } from 'antd';
+import ChatIcon from '../../assets/Icons/ChatIcon';
+import { Main } from '../BusinessProfile';
+import ScanIcon from '../../assets/Icons/ScanIcon';
+import SupportModal from '../../components/UI/Modal/UserSupportModal';
+import { TogleButton } from '../../components/Layout/BusinessSidebar';
+import { BsBoxArrowInLeft } from 'react-icons/bs';
+import UserSidebar from '../../components/Layout/UserSidebar';
+import { Html5Qrcode } from 'html5-qrcode';
 
 const UserProfile = () => {
   const [showSupportModal, setShowSupportModal] = useState(false);
@@ -34,10 +34,10 @@ const UserProfile = () => {
     isError,
     error,
     isSuccess,
-    refetch: refetchLocations
+    refetch: refetchLocations,
   } = useGetLocationsQuery({
     page: 1,
-    count: 10
+    count: 10,
   });
 
   const [userInfo, setUserInfo] = useState();
@@ -53,7 +53,7 @@ const UserProfile = () => {
       notification.error({
         message: error?.error,
         duration: 3,
-        placement: "bottomRight",
+        placement: 'bottomRight',
       });
     }
   }, [data, error?.error, isError, isSuccess, locations]);
@@ -69,30 +69,33 @@ const UserProfile = () => {
     }
   }, [location.pathname, firstVisit, refetchLocations]);
   const userLikedLocations = userInfo?.likedLocations;
-  
+
   // Filter out any undefined values in case a location name doesn't match any location
   const filteredUserLikedLocations = userLikedLocations?.filter(Boolean) || [];
 
   const logUserVisit = async (url) => {
-    const res = await fetch(`${process.env.REACT_APP_SERVER_URL}locations/visits?location=`, {
-      method: "PUT",
-      headers: {
-        "Authorization": `Bearer ${sessionStorage.getItem("authToken")}`
+    const res = await fetch(
+      `${process.env.REACT_APP_SERVER_URL}locations/visits?location=`,
+      {
+        method: 'PUT',
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem('authToken')}`,
+        },
       }
-    });
+    );
     const data = await res.json();
-    
+
     if (res.status === 201) {
       notification.success({
         message: data.message,
         duration: 3,
-        placement: "top",
+        placement: 'top',
       });
     } else {
       notification.error({
         message: data.message,
         duration: 3,
-        placement: "top",
+        placement: 'top',
       });
     }
   };
@@ -107,90 +110,99 @@ const UserProfile = () => {
       }
     }
 
-    html5QrCode = new Html5Qrcode("reader");
-    Html5Qrcode.getCameras().then(devices => {
-      if (devices && devices.length) {
-        let cameraId = devices[0].id;
-        if (window.innerWidth < 800) cameraId = devices[1].id;
+    html5QrCode = new Html5Qrcode('reader');
+    Html5Qrcode.getCameras()
+      .then((devices) => {
+        if (devices && devices.length) {
+          let cameraId = devices[0].id;
+          if (window.innerWidth < 800) cameraId = devices[1].id;
 
-        html5QrCode.start(
-          cameraId,
-          {
-            fps: 10,   // Optional, frame per seconds for qr code scanning
-            qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
-          },
-          (decodedText) => {
-            if (decodedText === "https://qr.link/Wk38Ni") {
-              logUserVisit(decodedText);
-              html5QrCode.stop();
+          html5QrCode.start(
+            cameraId,
+            {
+              fps: 10, // Optional, frame per seconds for qr code scanning
+              qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
+            },
+            (decodedText) => {
+              if (decodedText === 'https://qr.me-qr.com/WiGEwIwk') {
+                logUserVisit(decodedText);
+                html5QrCode.stop();
+              }
             }
-          })
-      }
-    })
-    .catch(err => console.error(err));
-  }
+          );
+        }
+      })
+      .catch((err) => console.error(err));
+  };
 
   return (
     <Container>
       <TogleButton showDashboard={showDashboard}>
-        <BsBoxArrowInLeft size={28} fill="black" onClick={() => setShowDashboard(prev => !prev)} />
+        <BsBoxArrowInLeft
+          size={28}
+          fill='black'
+          onClick={() => setShowDashboard((prev) => !prev)}
+        />
       </TogleButton>
       <UserSidebar showDashboard={showDashboard} userInfo={userInfo} />
       <Main>
-        <button className="fixed right-7 bottom-6 shadow-md rounded-full z-50 !scale-90" onClick={() => setShowSupportModal(true)}>
+        <button
+          className='fixed right-7 bottom-6 shadow-md rounded-full z-50 !scale-90'
+          onClick={() => setShowSupportModal(true)}
+        >
           <ChatIcon />
         </button>
         <button
-          className="fixed right-7 bottom-28 shadow-md rounded-full z-50 !scale-90 bg-[#FDEECE] p-2.5"
+          className='fixed right-7 bottom-28 shadow-md rounded-full z-50 !scale-90 bg-[#FDEECE] p-2.5'
           onClick={() => startScanningProccess()}
-          onBlur={() => (html5QrCode!== undefined) && html5QrCode.stop()}
+          onBlur={() => html5QrCode !== undefined && html5QrCode.stop()}
         >
           <ScanIcon />
         </button>
-        <div id="reader" width="560px" />
-        <div className="d-flex justify-content-between align-items-center mb-5 mt-3">
-          <div className="flex justify-start items-center gap-2 flex-wrap">
-            <Link to="/create-event">
-              <Button color="green">
-                Create an Event
-              </Button>
+        <div id='reader' width='560px' />
+        <div className='d-flex justify-content-between align-items-center mb-5 mt-3'>
+          <div className='flex justify-start items-center gap-2 flex-wrap'>
+            <Link to='/create-event'>
+              <Button color='green'>Create an Event</Button>
             </Link>
-            <Link to="/plan-a-trip">
+            <Link to='/plan-a-trip'>
               <Button>Plan A Trip</Button>
             </Link>
             <button
-              className="text-[#E9A309] font-semibold underline !scale-100"
+              className='text-[#E9A309] font-semibold underline !scale-100'
               onClick={() => navigate('/settings')}
             >
-              Settings{">"}
+              Settings{'>'}
             </button>
           </div>
         </div>
         <BoxContainer>
           {showSupportModal && (
-            <SupportModal username={userInfo?.username} onClick={() => setShowSupportModal((prev) => !prev)} />
+            <SupportModal
+              username={userInfo?.username}
+              onClick={() => setShowSupportModal((prev) => !prev)}
+            />
           )}
-          {
-            filteredUserLikedLocations.length < 1 ?
-              <p>
-                No Liked Locations Yet,{" "}
-                <Button onClick={() => navigate(`/business-locations`)}>
-                  Add some here{" "}
-                </Button>{" "}
-              </p>
-            : <GridContainer>
-                {filteredUserLikedLocations.map((location, i) => (
-                    <LocationBox
-                      onClick={() => {
-                        navigate(`/location/${location?._id}`);
-                      }}
-                      location={location}
-                      key={i}
-                    />
-                  )
-                )}
-              </GridContainer>
-          }
+          {filteredUserLikedLocations.length < 1 ? (
+            <p>
+              No Liked Locations Yet,{' '}
+              <Button onClick={() => navigate(`/business-locations`)}>
+                Add some here{' '}
+              </Button>{' '}
+            </p>
+          ) : (
+            <GridContainer>
+              {filteredUserLikedLocations.map((location, i) => (
+                <LocationBox
+                  onClick={() => {
+                    navigate(`/location/${location?._id}`);
+                  }}
+                  location={location}
+                  key={i}
+                />
+              ))}
+            </GridContainer>
+          )}
         </BoxContainer>
       </Main>
     </Container>
